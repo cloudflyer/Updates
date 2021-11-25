ba80a940b3f7c318f044de8d8233c5a0b1dd7362
cloudflyer <cloudflyer@users.noreply.github.com>
2021-11-25 06:35:02 +0100

    Update list_updates.sh

--- list_updates.sh
+++ list_updates.sh
@@ -1,19 +1,35 @@
 #!/bin/env bash
 deb="ubuntu01 ubuntu02"
-dnf="centos01"
+rpm="centos01"
 
-read -s -p "Enter sudo password: " mypassword
+sp='/-\|'
+sc=0
+spin() {
+    printf "\b${sp:sc++:1}"
+    ((sc==${#sp})) && sc=0
+    sleep 0.1
+}
+endspin() {
+    printf '\r%s\n' "$*"
+    sleep 0.1
+}
+
+read -s -p "Enter Password: " mypassword
 echo ""
 for i in $deb; do
+  spin
+  echo $mypassword | ssh -T $i.home "DEBIAN_FRONTEND=noninteractive sudo -S apt-get -qq update >/dev/null 2>&1 & disown"
+done
+sleep 3
+endspin
+for i in $deb; do
   echo $i
-  echo $mypassword | ssh -T $i.home "sudo -S apt-get -qq update"
-  #ssh $i.home "apt list --upgradable"
-  ssh -T $i.home "apt-show-versions -u"
+  ssh -T $i.home "DEBIAN_FRONTEND=noninteractive apt-show-versions -u"
   echo ""
 done
-for i in $dnf; do
+for i in $rpm; do
   echo $i
-  ssh $i.home "dnf check-update"
+  ssh $i.home "dnf check-update || yum check-update"
   echo ""
 done
 exit 0
