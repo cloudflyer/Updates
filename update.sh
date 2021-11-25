cb20656edaf9110122124138516272bab6dca945
cloudflyer <cloudflyer@users.noreply.github.com>
2021-11-25 06:38:06 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-PROGNAME="update.sh"
+PROGNAME=${0##*/}
 
 if test "$*" = "-?" -o "$*" = "-h" -o "$*" = "-help" -o "$*" = "--help" -o "x$*" = "x"
    then cat <<-ENDHELP                  # print file until ENDHELP
@@ -11,7 +11,7 @@ if test "$*" = "-?" -o "$*" = "-h" -o "$*" = "-help" -o "$*" = "--help" -o "x$*"
 #       This script will update the OS on:
 #       A specific server          (-deb server1)
 #       All debian based servers   (-deb all)
-#       All redhat based servers   (-dnf all)
+#       All redhat based servers   (-rpm all)
 #       All server                 (-all)
 #
 # Usage : $PROGNAME [-?][-test]
@@ -29,33 +29,29 @@ fi
 # --- HISTORY -------------------------------------------------------------------
 # Changes:
 #       Version yymmdd Who Changes - latest version first
-#       1.0     YYMMDD XXX Original
+#       1.0     190605 TGJ Original
+#       1.1     200202 TGJ Switched yum to dnf
+#       1.2     211112 TGJ Re-added yum support
 #
 # --- MAIN PROGRAM --------------------------------------------------------------
 
 deb="ubuntu01 ubuntu02"
-dnf="centos01"
+rpm="centos01"
 
 if [[ $1 == -all ]]; then
    if [ $# -eq 1 ]; then
-     read -s -p "Enter sudo password: " mypassword
+     read -s -p "Enter Password: " mypassword
      echo ""
      echo "Patching: $deb "
      for i in $deb; do
        echo $i
-       echo $mypassword | ssh -T $i.home "sudo -S apt-get -qq update"
-       #echo $mypassword | ssh -T $i.home "sudo -S apt list --upgradable"
-       echo $mypassword | ssh -T $i.home "sudo -S apt-show-versions -u"
-       echo $mypassword | ssh -T $i.home "sudo -S apt-get upgrade -y"
-       echo $mypassword | ssh -T $i.home "if [ -f /var/run/reboot-required ]; then cat /var/run/reboot-required; fi"
+       echo $mypassword | ssh -T $i.home "DEBIAN_FRONTEND=noninteractive sudo -S sh -c 'apt-get -qq update && apt-show-versions -u && apt-get upgrade -y && if [ -f /var/run/reboot-required ]; then cat /var/run/reboot-required; fi'"
        echo ""
      done
-     echo "Patching: $dnf "
-     for i in $dnf; do
+     echo "Patching: $rpm "
+     for i in $rpm; do
        echo $i
-       echo $mypassword | ssh -T $i.home "sudo -S dnf check-update"
-       echo $mypassword | ssh -T $i.home "sudo -S dnf update -y"
-       echo $mypassword | ssh -T $i.home "sudo -S needs-restarting  -r ; echo $?"
+       echo $mypassword | ssh -T $i.home "sudo -S -sh -c 'dnf check-update && dnf upgrade -y && needs-restarting  -r ; echo $?'"
        echo ""
      done
    fi
@@ -72,26 +68,18 @@ while [ $# -gt 1 ]; do
         -deb)
            if [ $# -eq 2 ]; then
               if [ $2 = "all" ]; then
-                 read -s -p "Enter sudo password: " mypassword
+                 read -s -p "Enter Password: " mypassword
                  echo ""
                  for i in $deb; do
                     echo $i
-                    echo $mypassword | ssh -T $i.home "sudo -S apt-get -qq update"
-                    #echo $mypassword | ssh -T $i.home "sudo -S apt list --upgradable"
-                    echo $mypassword | ssh -T $i.home "sudo -S apt-show-versions -u"
-                    echo $mypassword | ssh -T $i.home "sudo -S apt-get upgrade -y"
-                    echo $mypassword | ssh -T $i.home "if [ -f /var/run/reboot-required ]; then cat /var/run/reboot-required; fi"
+                    echo $mypassword | ssh -T $i.home "DEBIAN_FRONTEND=noninteractive sudo -S sh -c 'apt-get -qq update && apt-show-versions -u && apt-get upgrade -y && if [ -f /var/run/reboot-required ]; then cat /var/run/reboot-required; fi'"
                     echo ""
                  done
               else
-                 read -s -p "Enter sudo password for $2: " mypassword
+                 read -s -p "Enter Password for $2: " mypassword
                  echo ""
                  if ssh $2.home 'test -f "/usr/bin/apt"'; then
-                    echo $mypassword | ssh -T $2.home "sudo -S apt-get -qq update"
-                    #echo $mypassword | ssh -T $2.home "sudo -S apt list --upgradable"
-                    echo $mypassword | ssh -T $2.home "sudo -S apt-show-versions -u"
-                    echo $mypassword | ssh -T $2.home "sudo -S apt-get upgrade -y"
-                    echo $mypassword | ssh -T $2.home "if [ -f /var/run/reboot-required ]; then cat /var/run/reboot-required; fi"
+                    echo $mypassword | ssh -T $2.home "DEBIAN_FRONTEND=noninteractive sudo -S sh -c 'apt-get -qq update && apt-show-versions -u && apt-get upgrade -y && if [ -f /var/run/reboot-required ]; then cat /var/run/reboot-required; fi'"
                     echo ""
                  else
                     echo "Unable to run the apt-command, is this a debian-based server? "
@@ -106,34 +94,38 @@ while [ $# -gt 1 ]; do
         shift
         shift
         ;;
-        -dnf)
+        -rpm)
            if [ $# -eq 2 ]; then
               if [ $2 = "all" ]; then
-                 read -s -p "Enter sudo password: " mypassword
+                 read -s -p "Enter Password: " mypassword
                  echo ""
-                 for i in $dnf; do
+                 for i in $rpm; do
                     echo $i
-                    echo $mypassword | ssh -T $i.home "sudo -S dnf check-update"
-                    echo $mypassword | ssh -T $i.home "sudo -S dnf update -y"
-                    echo $mypassword | ssh -T $i.home "sudo -S needs-restarting  -r ; echo $?"
-                    echo ""
+                    if ssh $i.home 'test -f "/usr/bin/dnf"'; then
+                       echo $mypassword | ssh -T $i.home "sudo -S sh -c 'dnf check-update && dnf update -y && needs-restarting  -r ; echo $?'"
+                       echo ""
+                    else
+                       echo $mypassword | ssh -T $i.home "sudo -S sh -c 'yum check-update && yum update -y && needs-restarting  -r ; echo $?'"
+                       echo ""
+                    fi
                  done
               else
-                 read -s -p "Enter sudo password for $2: " mypassword
+                 read -s -p "Enter Password for $2: " mypassword
                  echo ""
                  if ssh $2.home 'test -f "/usr/bin/dnf"'; then
-                    echo $mypassword | ssh -T $2.home "sudo -S dnf check-update"
-                    echo $mypassword | ssh -T $2.home "sudo -S dnf update -y"
-                    echo $mypassword | ssh -T $2.home "sudo -S needs-restarting  -r ; echo $?"
+                    echo $mypassword | ssh -T $2.home "sudo -S sh -c 'dnf check-update && dnf update -y && needs-restarting -r ; echo $?'"
+                    echo ""
+                 elif ssh $2.home 'test -f "/usr/bin/yum"'; then
+                    echo $mypassword | ssh -T $2.home "sudo -S sh -c 'yum check-update && yum update -y && needs-restarting -r ; echo $?'"
                     echo ""
                  else
-                    echo "Unable to run the dnf-command, is this a redhat-based server? "
+                    echo "Unable to run the dnf/yum command, is this a redhat-based server? "
                     exit 1
                  fi
               fi
            else
-              echo "You need to enter an argument after '-dnf' "
-              echo "Ex. -dnf server1 "
+              echo "You need to enter an argument after '-rpm' "
+              echo "Ex. -rpm server1 "
               exit 1
            fi
         shift
